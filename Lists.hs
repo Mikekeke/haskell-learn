1aac15f511f3beb18b15069ac02dd99c256c0d4f
Mikhel <m282021@gmail.com>
2017-10-02 23:39:41 +0300

    ...

--- Lists.hs
+++ Lists.hs
@@ -23,3 +23,54 @@ concat' :: [a] -> [a] -> [a]
 isEmpty :: [a] -> Bool
 isEmpty [] = True
 isEmpty _ = False
+
+oddsOnly :: Integral a => [a] -> [a]
+oddsOnly [] = []
+oddsOnly (x: xs)
+    | odd x = x : oddsOnly xs
+    | otherwise = oddsOnly xs
+
+init' :: [a] -> [a]
+init' []     = error "Empty list"
+init' (x:[]) = []
+ -- !!! or better init' [x] = []
+ -- !!! or even better init' [_] = []
+init' (x:xs) = x : init' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' [x] = x
+sum' (x:xs) = x + sum' xs
+
+maxIn :: (Ord a) => [a] -> a
+maxIn [] = error "Empty list"
+maxIn [x] = x
+maxIn (x:xs) = x `max` maxIn xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' [x] = [x]
+reverse' xs = (last xs) : reverse' (init xs)
+
+reverse'' :: [a] -> [a]
+reverse'' [] = []
+reverse'' [x] = [x]
+reverse'' l =
+    let
+        go [] acc = acc
+        go (x:xs) acc = go  xs (x : acc)
+    in
+        go l []
+
+isPalindromeMy :: Eq a => [a] -> Bool
+isPalindromeMy [] = True
+isPalindromeMy [x] = True
+isPalindromeMy (x:xs) = (x == last xs) && isPalindrome (init xs)
+
+-- missed this again lol
+isPalindrome1 :: (Eq a) => [a] -> Bool
+isPalindrome1 xs = xs == (reverse xs)
+
+isPalindrome2 []  = True
+isPalindrome2 [_] = True
+isPalindrome2 xs  = (head xs) == (last xs) && (isPalindrome' $ init $ tail xs)
