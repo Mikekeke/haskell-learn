0f58ef8a9e1a368b44ff4a842f0f708090c1861c
m282021@gmail.com <zDmVJe6D>
2017-12-18 17:53:44 +0300

    ...

---
+++ Folds.hs
@@ -0,0 +1,21 @@
+lengthList :: [a] -> Int
+lengthList = foldr (\_ acc -> acc + 1)(0)
+
+lengthList':: [a] -> Int
+lengthList'= foldr f (0) where
+  f _ acc = acc + 1
+
+lengthList'' :: [a] -> Int
+lengthList'' = foldr (const (+1)) 0
+
+
+sumOdd :: [Integer] -> Integer
+sumOdd = foldr (\x s -> if odd x then x + s else s) 0
+
+sumOdd' :: [Integer] -> Integer
+sumOdd' = foldr f 0 where
+  f x s | odd x     = x + s
+        | otherwise = s
+
+sumOdd'' :: [Integer] -> Integer
+sumOdd'' = foldr (+) 0 . filter odd
