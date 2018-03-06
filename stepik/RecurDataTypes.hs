d95bc0477d6839e1354234d372dad8cdce0cf55c
Mikhel <m282021@gmail.com>
2018-03-06 23:58:56 +0300

    ...

--- stepik/RecurDataTypes.hs
+++ stepik/RecurDataTypes.hs
@@ -59,6 +59,35 @@ height (Leaf _) = 0
 height (Node l r) = 1 + max (height l) (height r)
 
 size :: Tree a -> Int
-size (Leaf _) = 0
+size (Leaf _) = 1
 size (Node l r) = 1+ (size l) + (size r)
 
+avg :: Tree Int -> Int
+avg t =
+    let (c,s) = go t
+    in s `div` c
+  where
+    go :: Tree Int -> (Int,Int)
+    go (Leaf x) = (1,x)
+    go (Node l r) = (c1+c2, s1+s2) where 
+        (c1,s1) = go l
+        (c2,s2) = go r
+        
+
+--EXPR
+infixl 6 :+:
+infixl 7 :*:
+data Expr = Val Int | Expr :+: Expr | Expr :*: Expr
+    deriving (Show, Eq)
+
+expand :: Expr -> Expr
+-- expand ((e1 :+: e2) :*: e) = expand e1 :*: expand e :+: expand e2 :*: expand e
+-- expand (e :*: (e1 :+: e2)) = expand e :*: expand e1 :+: expand e :*: expand e2
+expand (e1 :+: e2) = expand e1 :+: expand e2
+expand (Val a :*: Val b) = Val a :*: Val b
+expand (e1 :*: e2) = Val 100
+expand e = e
+
+tst = (Val 1 :+: Val 2 :+: Val 3) :*: (Val 4 :+: Val 5)
+correct = Val 1 :*: Val 4 :+: (Val 1 :*: Val 5 :+: (Val 2 :*: Val 4 :+: (Val 2 :*: Val 5 :+: (Val 3 :*: Val 4 :+: Val 3 :*: Val 5))))
+check = expand tst == expand correct 
