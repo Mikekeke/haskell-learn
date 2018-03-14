05e62fa410ed8487bce5c80e5b6eb06a3cb64492
Mikhel <m282021@gmail.com>
2018-03-14 23:56:19 +0300

    ...

--- tests/Tst.hs
+++ tests/Tst.hs
@@ -1,16 +1,23 @@
-import System.IO
-import Data.List
-import Data.Char
+import           Data.Char
+import           Data.List
+import           System.IO
 
 l1 = [1,2,3]
 l2 = [4,5]
-f:: [Int] -> [Int] -> [Int]
+fn:: [Int] -> [Int] -> [Int]
 fn = (:).head
-ss :: [Int] -> [Int]
 
-dd:::: (Applicative f) => f (a -> b) -> f a -> f b
+dd:: Applicative f => f (a -> b) -> f a -> f b
 dd = (<*>)
 
+ss :: [Int] -> [Int]
 ss = fn <*> tail
 
 res = fn l1 l2
+
+
+func a b
+    | Nothing <- a = -1
+    | Nothing <- b = -2
+    | Nothing <- a, Nothing <- b = -3
+    | (Just x) <- a, (Just y) <- b = x+y
