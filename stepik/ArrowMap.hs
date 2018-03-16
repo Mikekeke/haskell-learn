e2a8c483aec0226faf4001fdedefb74bbd953bba
mike <m282021@gmail.com>
2018-03-16 18:17:41 +0300

    ....

--- stepik/ArrowMap.hs
+++ stepik/ArrowMap.hs
@@ -1,5 +1,7 @@
-import qualified Data.List as L
-import           Prelude   hiding (lookup)
+-- import qualified Data.List as L
+-- import           Prelude   hiding (lookup)
+
+import Debug.Trace
 
 class MapLike m where
     empty :: m k v
@@ -10,12 +12,20 @@ class MapLike m where
 
 newtype ArrowMap k v = ArrowMap { getArrowMap :: k -> Maybe v }
 
+-- didn't solve myself, idea was the same, but for whatewer (synthax) reason it didnt work
 instance MapLike ArrowMap where
-    empty = ArrowMap $ \_-> Nothing
-
+    empty = ArrowMap $ const Nothing
+    lookup key (ArrowMap f) = f key
+    insert key value (ArrowMap f) = ArrowMap (\k -> if k == key then Just value else f k)
+    delete key (ArrowMap f) = ArrowMap (\k -> if k == key then Nothing else f k)
 
     fromList []         = empty
     fromList ((k,v):xs) = insert k v (fromList xs)
-
-
-f = \x -> case x of {42 -> Just "ANSWER"; 0 -> Just "ZERO"; _ -> Nothing}
+    
+-- stuff version
+-- instance MapLike ArrowMap where
+--     empty = ArrowMap $ const Nothing
+--     lookup k (ArrowMap f) = f k
+--     insert k v (ArrowMap f) = ArrowMap $ \x -> if x == k then return v else f x
+--     delete k (ArrowMap f) = ArrowMap $ \x -> if x == k then Nothing else f x
+--     fromList = foldr (uncurry insert) empty
