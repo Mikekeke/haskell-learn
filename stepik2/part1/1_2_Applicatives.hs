cceb6f0189eb80525eadc7f0f81b9d7e1ac273a9
mike <m282021@gmail.com>
2018-04-26 18:00:57 +0300

    ...

--- stepik2/part1/1_2_Applicatives.hs
+++ stepik2/part1/1_2_Applicatives.hs
@@ -7,3 +7,13 @@ x1s = [1,2,3]
 x2s = [4,5,6]
 x3s = [7,8,9]
 x4s = [10,11,12]
+
+-- step 7
+divideList :: Fractional a => [a] -> a
+divideList []     = 1
+divideList (x:xs) = (/) x (divideList xs)
+
+divideList' :: (Show a, Fractional a) => [a] -> (String,a)
+divideList' []     = ("1.0",1)
+divideList' (x:xs) = (/) <$> (toLogEntry x, x) <*> divideList' xs where
+    toLogEntry x' = "<-" ++ show x' ++ "/"
