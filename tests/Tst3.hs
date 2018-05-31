6c2272819cfc73ceb6bd0e83c6cfea16f7815247
Mikhel <m282021@gmail.com>
2018-06-01 00:34:32 +0300

    ...

--- tests/Tst3.hs
+++ tests/Tst3.hs
@@ -1,3 +1,5 @@
+import           Control.Monad
+
 find' :: (a -> Bool) -> [a] -> Maybe a
 find' p = foldr (\x z -> if p(x) then Just x else z) Nothing
 
@@ -8,8 +10,10 @@ fn2 x | x > 3 = Right x
       | otherwise = Left "error fn2"
 
 t1 x = fn1 x >>= fn2
+t1' = fn1 >=> fn2
 
 newtype Vald a b = Vald {getEth :: Either a b}
+
 instance Functor (Vald a) where
     fmap f vld = Vald $ fmap f (getEth vld)
 
@@ -19,9 +23,3 @@ instance Monoid a => Applicative (Vald a) where
     (Vald (Left s1)) <*> (Vald (Right _)) = Vald $ Left s1
     (Vald (Left m1)) <*> (Vald (Left m2)) = Vald $ Left $ mappend m1 m2
 
-
-
--- instance Monoid a => Monad (Vald a) where
---     return = Vald . Right
---     (Vald (Right x)) >>= k = k x
---     (Vald (Left x)) >>= _ = Vald (Left x)
