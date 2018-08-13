ef8c5b0738d02041d4c1afd61f9a8e82e9c00bf2
mike <m282021@gmail.com>
2018-08-13 18:15:01 +0300

    ...

--- stepik2/part1/1_5_Type_Composition.hs
+++ stepik2/part1/1_5_Type_Composition.hs
@@ -3,6 +3,9 @@ module TypeComposition where
     infixr 9 |.|
     newtype (|.|) f g a = Cmps {getCmps ::  f (g a)} deriving (Eq, Show)
 
+    -- :t Cmps (Just "abc") :: (Maybe |.| []) Char 
+    --    ~> (Just "abc") :: (Maybe |.| []) Char :: (|.|) Maybe [] Char
+
     type A   = ((,) Integer |.| (,) Char) Bool
     type B t = ((,,) Bool (t -> t) |.| Either String) Int
     type C   = (|.|) ((->) Bool) ((->) Integer) Integer
@@ -19,4 +22,16 @@ module TypeComposition where
     c1 = Cmps seq
     c2 = Cmps $ flip const
     c3 = Cmps $ const $ const 3
+
+    instance (Functor f, Functor g) => Functor (f |.| g) where
+    --  fmap :: (a -> b) -> (f |.| g) a -> (f |.| g) b  
+    --  fmap :: (a -> b) -> (|.|) f g a -> (|.|) f g b  
+        fmap fn cmps = Cmps $ (fmap.fmap) fn (getCmps cmps) 
+    --  fmap fn (Cmps x) = Cmps $ fmap (fmap fn) x 
+
+    newtype Cmps3 f g h a = Cmps3 { getCmps3 :: f (g (h a)) } deriving (Eq,Show) 
+    
+    instance (Functor f, Functor g, Functor h) => Functor (Cmps3 f g h) where
+        fmap fn = Cmps3 . (fmap.fmap.fmap $ fn) . getCmps3
+        -- fmap = (Cmps3 .) . (. getCmps3) . fmap . fmap . fmap
 
