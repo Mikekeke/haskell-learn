dce3c88f3e39a0a236dffab576d5d83053a3f319
mike <m282021@gmail.com>
2018-08-13 16:26:16 +0300

    ...

--- tests/Tst8.hs
+++ tests/Tst8.hs
@@ -1,3 +1,4 @@
+import Data.Monoid
 
 group' :: Eq a => [a] -> [[a]]
 group' [] = []
@@ -9,3 +10,22 @@ group' (x:xs) | x == head xs = let (x':xs') = group' xs in (x : x') : xs'
 --   λ: 1:2:[3]
 --   [1,2,3]
 
+data Validate e a = Invalid {errors :: e} | Valid a deriving Show
+instance Functor (Validate e) where
+    fmap f (Valid v) = Valid $ f v
+    fmap _ (Invalid es) = Invalid es
+
+instance Monoid e => Applicative (Validate e) where
+    pure = Valid
+    (Valid f) <*> (Valid v) = Valid $ f v
+    (Invalid e1) <*> (Invalid e2) = Invalid $ e1 <> e2
+    (Invalid e) <*> (Valid _) = Invalid e
+    (Valid _) <*> (Invalid e) = Invalid e
+
+data Obj = Obj Int String deriving Show
+prsInt x = if x >= 10 then Valid x else Invalid ["error: x < 10"]
+prsStr s = if (length s) < 5 then Invalid ["str too short"] else Valid s
+parseObj x s = Obj <$> prsInt x <*> prsStr s
+-- tstGood = (. prsStr) . (<*>) . (Obj <$>) . prsInt
+tstGood = parseObj 12 "sobaka"
+tstBad = parseObj 3 "kek"
