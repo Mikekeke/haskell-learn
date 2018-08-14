b63e1d51c1c5e8e7baafdcb217245f031c4c3a4c
mike <m282021@gmail.com>
2018-08-14 18:29:44 +0300

    ...

--- tests/Tst8.hs
+++ tests/Tst8.hs
@@ -29,3 +29,19 @@ parseObj x s = Obj <$> prsInt x <*> prsStr s
 -- tstGood = (. prsStr) . (<*>) . (Obj <$>) . prsInt
 tstGood = parseObj 12 "sobaka"
 tstBad = parseObj 3 "kek"
+
+sliding :: Int -> [a] -> [[a]]
+
+sliding 0 _ = []
+sliding _ [] = []
+sliding n ls | length ls <= n = [ls]
+             | otherwise = go n ls where
+                go n' (x':xs') | n' == 0 = [] :  sliding n (tail ls)
+                               | otherwise = let (y:ys) = go (n' - 1) xs' in (x' : y) : ys
+
+sliding' :: Int -> [a] -> [[a]]
+sliding' n ls | n == 0 || null ls = []
+              | length ls > n =  (take n ls)  : sliding' n (tail ls)
+              | otherwise = [ls]
+
+slTest x = (==) <$> sliding x <*> sliding' x
