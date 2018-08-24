b578b4bd3ebeb2409813cc6cc83860d25a253559
mike <m282021@gmail.com>
2018-08-24 16:26:13 +0300

    ...

--- tests/Tst8.hs
+++ tests/Tst8.hs
@@ -1,5 +1,7 @@
 import           Data.Monoid
 
+import Debug.Trace
+
 group' :: Eq a => [a] -> [[a]]
 group' [] = []
 group' [x] = [[x]]
@@ -39,3 +41,35 @@ sliding p n ls | length ls <= n = [ls]
                 go n' (x:xs) | n' == 0 = [] :  sliding p n (tail ls)
                              | otherwise = let (y:ys) = go (n' - 1) xs in
                                 if p x (head y) then (x : y) : ys else []
+
+-- http://hackage.haskell.org/package/base-4.11.1.0/docs/src/Data.Foldable.html#foldlM
+foldlM :: (Foldable t, Monad m) => (b -> a -> m b) -> b -> t a -> m b
+foldlM f z0 xs = foldr f' return xs z0
+  where f' x k z = f z x >>=  k
+
+isBalanced' xs = foldlM op [] xs
+  where
+    op ('(':xs) ')'          = trace "j" Just xs
+    op ('[':xs) ']'          = trace "j" Just xs
+    op ('{':xs) '}'          = trace "j" Just xs
+    op xs x | x `elem` ")]}" = trace "n" Nothing
+            | otherwise      = trace "j" Just (x:xs)
+
+
+bnd :: Maybe a -> (a -> Maybe b) -> Maybe b
+bnd (Just v) k = k v
+bnd Nothing k = trace "n" Nothing
+tst = trace "1" Just 4 `bnd` trace "2" const Nothing  `bnd` trace "3" const Nothing  `bnd` trace "4" const Nothing
+-- > tst
+-- 1
+-- 2
+-- n
+-- n
+
+foldlMm :: Foldable t => (b -> a -> Maybe b) -> b -> t a -> Maybe b
+foldlMm f z0 xs = foldr f' return xs z0
+  where f' x k z = f z x `bnd` k
+
+-- а вот здесь 'n' не печатается для `bnd` после 1го Nothing
+tst1 = foldlMm fn 0 [1..10000000] where fn z0 x = if x < 10 then Just (z0+x) else Nothing
+tst2 = foldlMm fn 0 [1..10000000] where fn z0 x = if x < 1000000 then Just (z0+x) else Nothing
