84003ad04afd7518b9de5b8b0e1bef3a6fff0cad
Mikhel <m282021@gmail.com>
2018-08-26 21:52:08 +0300

    ....

--- algs_and_structures/part1/1_1_BracketsSimple.hs
+++ algs_and_structures/part1/1_1_BracketsSimple.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE ViewPatterns #-}
 import Control.Monad (foldM)
+import Data.Char
 import Debug.Trace
+import Data.Maybe
 
 
 newtype Stack a = Stack {getSt :: [a]} deriving (Show, Eq)
@@ -19,6 +21,10 @@ pop (Stack (x:xs)) = (Just x, Stack xs)
 top :: Stack a -> Maybe a
 top (Stack [])    = Nothing
 top (Stack (x:_)) = Just x
+topUnsafe = fromJust . top
+
+size :: Stack a -> Int
+size = length . getSt
 
 
 
@@ -33,17 +39,30 @@ top (Stack (x:_)) = Just x
 
 type Result = Either Int String
 ini = (0, emptyStack)
-resultCheck (cnt, s) = if isEmpty s then "Success" else show cnt
+resultCheck (cnt, s) = if isEmpty s then "Success" else show $ size s
+closing = [')',']','}']
+opening = ['(','[','{']
+brackets = closing ++ opening
 isClosing :: Char -> Bool
-isClosing = flip elem [')',']','}']
+isClosing = flip elem closing
+isOpening = not . isClosing
+isValidPair = flip elem [('(',')') , ('[',']'), ('{','}') ]
 
+
+-- check bs = either show (resultCheck) $ foldM f ini bs
 check bs = either (show :: Int -> String) (resultCheck) $ foldM f ini bs
         where
-            f (cnt, stack) bracket  = case flip (,) bracket <$> top stack of
-                Just ('(',')')              -> Right(succ cnt, snd $ pop stack)
-                Just ('[',']')              -> Right(succ cnt, snd $ pop stack)
-                Just ('{','}')              -> Right(succ cnt, snd $ pop stack)
-                Just (isClosing -> False,_) -> Right (succ cnt, push bracket stack)
-                Nothing                     -> Right (succ cnt, push bracket stack)
-                _                           -> Left (succ cnt) 
+            f (cnt, stack) b | not (elem b brackets) = Right(succ cnt, stack)
+                             | isEmpty stack && isClosing b = Left (succ cnt) 
+                             | isEmpty stack || isOpening b  = Right (succ cnt, push b stack)
+                             | isValidPair (topUnsafe stack, b) = Right(succ cnt, snd $ pop stack)
+                             | otherwise = Left (succ cnt)
 
+testInputs = ["([](){([])})", "()[]}", "{{[()]]"]
+testOuts = ["Success", "5", "7"]
+test = zipWith (==) testOuts (map check testInputs)
+
+check "() ( {}" надо 3, получается 1
+
+main :: IO ()
+main = getLine >>= putStrLn . check
