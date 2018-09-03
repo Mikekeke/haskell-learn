7059942cddb6e863efd09abddd1754371f5ef537
Mikhel <m282021@gmail.com>
2018-09-04 01:18:24 +0300

    ,,,

--- codewars/Part1.hs
+++ codewars/Part1.hs
@@ -1,4 +1,9 @@
 import           Debug.Trace
+import Control.Applicative ((<$>), (<*>))
+import Data.List (elemIndex, sortBy)
+import Data.Maybe (fromMaybe)
+import Data.Char (isDigit)
+import Data.Ord (comparing)
 
 fact x = product [1..x]
 
@@ -10,13 +15,13 @@ f x
 zeros :: Int -> Int
 zeros x = foldl (\b a -> b + (f a))  0  [1..x]
 
--- String repeat
+-- String repeat --
 repeatStr :: Int -> String -> String
 repeatStr n str = mconcat $ replicate n str
 -- cool effect'ish from solutions 
 -- repeatStr n str = [1..n] >> str
 
--- Equal Sides Of An Array
+-- Equal Sides Of An Array --
 -- https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/haskell
 findEvenIndex :: [Int] -> Int
 findEvenIndex [] = 0
@@ -30,3 +35,13 @@ findEvenIndex (x:xs) | sum xs == 0 = 0
                                                            | null xs' = -1
                                                            | otherwise = go (l+x') (r - head xs') (succ cnt) xs'
                                     in go iniL iniR 1 xs
+-- best answer, much faster and less memory than mine
+findEvenIndex1 :: [Int] -> Int
+findEvenIndex1 = fromMaybe (-1) . elemIndex True .
+  (zipWith (==) <$> scanl1 (+) <*> scanr1 (+))
+
+-- yourOrderPlease -- 
+{- sorts string like "f2 gg0 dd1" by digit inside word-}
+pullNum s = head . dropWhile (not . isDigit) $ s
+yourOrderPlease :: String -> String
+yourOrderPlease = unwords . sortBy (comparing pullNum) . words
