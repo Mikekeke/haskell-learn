01f5978f6338a0fa58e8a3bea793ddf6767b6329
Mikhel <m282021@gmail.com>
2018-10-01 00:31:57 +0300

    ...

--- my/TouchingMonoids.hs
+++ my/TouchingMonoids.hs
@@ -1,3 +1,11 @@
+class Monoid1 a where
+    memptyest :: a
+    mappendy :: a -> a -> a
+
+instance Monoid1 b => Monoid1 (a -> b) where
+    memptyest _ = memptyest
+    mappendy f g x = f x `mappendy` g x
+
 -- λ: import Data.Monoid
 -- λ: :t [(+2), (*10)]
 -- [(+2), (*10)] :: Num a => [a -> a]
@@ -15,3 +23,17 @@
 -- Product {getProduct = 30}
 -- λ: (appEndo . mconcat . map Endo) [(+2), (*10)] 3
 -- 32
+-- λ: mappend (+3) (*2) (Sum 2)
+-- Sum {getSum = 9}
+-- λ: mappend (+3) (*2) (Product  2)
+-- Product {getProduct = 20}
+-- λ: mempty (Sum 3)
+-- ()
+
+-- λ: import Data.Foldable
+-- λ: :t fold
+-- fold :: (Monoid m, Foldable t) => t m -> m
+-- λ: fs = Endo <$> [(+2), (*3)]
+-- λ: :t fold fs
+-- fold fs :: Num a => Endo a
+-- λ: appEndo (fold fs) 3
