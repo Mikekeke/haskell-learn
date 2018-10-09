902516555fa88b8641f11bb541dd8bfd109d1208
mike <m282021@gmail.com>
2018-10-09 18:15:47 +0300

    ...

---
+++ my/Essential4.hs
@@ -0,0 +1,10 @@
+null' :: [a] -> Bool
+null' = foldr (\_ _ -> False) True
+--              ^^^ оба значения попадают в функцию и игнорируются
+{-
+f = (\_ _ -> False)
+null [1,2]
+~> foldr f True [1,2]
+~> f 1 (foldr f True [2])
+~> False
+-}
