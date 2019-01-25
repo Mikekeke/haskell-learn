8a3a8907edaa1ef0e7c4837e9784dfffda1ef55d
Mikhel <m282021@gmail.com>
2019-01-26 01:10:10 +0300

    ...

--- stepik2/part3/3_2_ContMonad.hs
+++ stepik2/part3/3_2_ContMonad.hs
@@ -1,3 +1,5 @@
+import Control.Monad (ap, return)
+
 -- decode c [x] = x
 -- decode c (x:xs) = foldl (\b x -> if b < x then b*x else x+b) x xs
 
@@ -21,7 +23,40 @@ hundred = cpsfy 100
 thousand = cpsfy 1000
 
 -- ff = decode twenty as a number
-ff = decode one hundred twenty three as a number
-{-
+test1 = decode one hundred twenty three as a number
 
--}
+
+newtype Cont r a = Cont {runCont :: (a -> r) -> r}
+
+showCont :: Show a => Cont String a -> String
+showCont m = runCont m show
+-- from answers : showCont = (`runCont` show)
+
+ret :: a -> Cont r a
+ret x = Cont $ \c -> c x 
+
+
+instance Functor (Cont r) where
+    {-
+    f :: a -> b
+    g :: (a -> r) -> r
+    c :: b -> r
+    -}
+
+    fmap f (Cont g) = Cont $ \c -> g (\a1 -> c (f a1))
+
+instance Applicative (Cont r) where
+    pure x = Cont $ \c -> c x
+    {-
+    apF :: ((a -> b) -> r) -> r
+    f :: (a -> r) -> r
+    c :: b -> r
+    -}
+    (Cont fa) <*> (Cont f) = 
+        Cont $ \c -> f (\a1 -> fa(\g -> c (g a1)))
+
+instance Monad (Cont r) where
+    return x = Cont $ \c -> c x
+--  (>>=) :: Cont r a -> (a -> Cont r b) -> Cont r b
+    (Cont f) >>= k = Cont $ \c -> f (\a1 -> runCont (k a1) c)
+        
