3ae277f3cfd3480de5d7a4a07060504e1a32ca03
Mikhel <m282021@gmail.com>
2019-01-28 23:06:30 +0300

    ...

---
+++ tests/TstCont.hs
@@ -0,0 +1,42 @@
+import Control.Monad.Cont
+import Debug.Trace
+
+ex1 :: Cont Int Int
+ex1 = do
+    a <- return 1
+    b <- cont (\f -> (f a) + (f a))
+    c <- return 3
+    return $ traceShow (a,b,c) $ a + b + c
+
+
+
+testl1 = [3,2,1,0,1,2,3,4]
+testl2 = [1,2,3]
+prod :: [Integer] -> Integer
+prod [] = 0
+prod (x:[]) = x
+prod (x:xs) | x == 0 = 0
+            | otherwise = trace "mult" $ x * (prod xs)
+
+prodContStep :: Integer -> Cont Integer Integer
+-- prodContStep 0 = cont (\_ -> 0) 
+prodContStep 0 = cont (const 0) 
+prodContStep x = return x
+
+prodCnt [] = cont (\_ -> 0)
+prodCnt (x:[]) = return x
+prodCnt (x:xs) = do
+    v1 <- prodContStep x
+    rest <- prodCnt xs
+    return $ trace "cont mult" $ v1 * rest
+
+tstProdCont l = runCont (prodCnt l) id
+
+{-
+Way to do efficient product with Cont:
+"tstProdCont testl1" should not trace any "cont mult"
+coz will pass "cont (\_ -> 0)" instead all preceeding computation when encounters 0
+unlike "prod", which will still perform 3 multiplications till reach 0
+
+how base does "product" can't understand yet
+-}
