13bc4a99be43e2e3012a30d9260624986c140080
Mikhel <m282021@gmail.com>
2019-02-14 22:52:28 +0300

    ...

--- stepik2/part3/3_3_Transformers_p2.hs
+++ stepik2/part3/3_3_Transformers_p2.hs
@@ -18,21 +18,32 @@ runEsSi = runState . runExceptT
 
 go :: Integer -> Integer -> State Integer Integer -> EsSi ()
 go lowerBound upperBound st = do 
-  e <- lift get undone
-  let x = evalState st (traceShowId e)
-  when ((traceShowId x) <= lowerBound) (throwE "Lower bound")
-  when (x >= upperBound) (throwE "Upper bound")
+    lift (modify $ execState st)
+    s <- lift get
+    when (s <= lowerBound) (throwE "Lower bound")
+    when (s >= upperBound) (throwE "Upper bound")
 
+    
 {-
 forever     :: (Applicative f) => f a -> f b
 forever a   = let a' = a *> a' in a'
 
-GHCi> runEsSi (go 1 85 tickCollatz) 27
-(Right (),82)
-GHCi> runEsSi (go 1 80 tickCollatz) 27
-(Left "Upper bound",82)
-GHCi> runEsSi (forever $ go 1 1000 tickCollatz) 27
-(Left "Upper bound",1186)
-GHCi> runEsSi (forever $ go 1 10000 tickCollatz) 27
-(Left "Lower bound",1)
+-}
+
+tst1 = runEsSi (go 1 85 tickCollatz) 27                           == (Right (),82)
+tst2 = runEsSi (go 1 80 tickCollatz) 27                           == (Left "Upper bound",82)
+tst3 = runEsSi ((forever $ go 1 1000 tickCollatz) :: EsSi ()) 27  == (Left "Upper bound",1186)
+tst4 = runEsSi ((forever $ go 1 10000 tickCollatz) :: EsSi ()) 27 == (Left "Lower bound",1)
+tsts = mapM_ (putStrLn . show) $ zip [1..] [tst1, tst2, tst3, tst4]
+
+{-
+!!! stuffs solution
+!!! lift :: (MonadTrans t, Monad m) => m a -> t m a
+
+go :: Integer -> Integer -> State Integer Integer -> EsSi ()
+go lower upper next = do
+  lift next -- Лифтить можно любое вычисление во внутренней монаде
+  n <- lift get
+  when (n <= lower) (throwE "Lower bound")
+  when (n >= upper) (throwE "Upper bound")
 -}
