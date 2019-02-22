249fde76d524c46b549b12ea4150bb4f0d7a62b8
Mike L <m282021@gmail.com>
2019-02-22 13:35:47 +0300

    ...

--- my/genetic_runner/Runner.hs
+++ my/genetic_runner/Runner.hs
@@ -1,9 +1,12 @@
-import Control.Monad.State
 import Data.List
-import Control.Monad.Except
 import Control.Monad
+import Control.Monad.State
+import Control.Monad.Except
+import Data.Maybe
+import Control.Applicative
 
 data Tile = Flat | Rock | Peak deriving Show
+type Track = [Tile]
 data Move = Run | Jump | DoubleJump deriving Show
 data Runner = Runner {rID :: String, energy :: Int, moveSet :: [Move]} deriving Show
 data RunnerRank = RunnerRank {runnerId :: String, rank :: Int} deriving Show
@@ -12,27 +15,53 @@ testTrack = [Flat, Flat, Peak, Rock, Flat]
 testRunner = Runner {
     rID = "r1"
     , energy = 6
-    , moveSet = [Run, Run, DoubleJump, Jump, Run]
+    , moveSet = [Run, Run, Run, Run]
 }
 initRank runner = RunnerRank (rID testRunner) 0
 testState = (testRunner, testTrack, initRank testRunner)
 
-runnerDone runner | energy runner == 0 = True
-                  | otherwise = False
+noEnergy rnr | energy rnr == 0 = True
+             | otherwise = False
+noMoves = null . moveSet
+
+nextMove rnr = case moveSet rnr of
+    [] -> Left "No moves left"
+    x:_ -> Right x
+nextTile tr = case tr of
+    [] -> Left "Track ended"
+    x:_ -> Right x
+
+canPass :: Move -> Tile -> Bool 
+canPass Run Flat = True
+canPass Jump Rock = True
+canPass DoubleJump Peak = True
+canPass _ _ = False
 
 trackEnded :: [Tile] -> Bool
 trackEnded = null
 
-changeState (runner, track, currentRank) = (
-    runner {energy = (pred $ energy runner), moveSet = tail (moveSet runner)}
-    , tail track
-    , currentRank {rank = succ $ rank currentRank} )
+tickRunner rnr = rnr {energy = (pred $ energy rnr), moveSet = tail (moveSet rnr)}
+tickRank cr = cr {rank = succ $ rank cr}
 
-type RunnerApp = ExceptT String (State (Runner, [Tile], RunnerRank)) ()
-makeMove :: RunnerApp
-makeMove = do
-    (runner, track, rank) <- get
-    when (trackEnded track) (throwError "Track ended")
-    when (runnerDone runner) (throwError "Runner done")
-    modify changeState
+tryTrack rnr tr = do
+    nextM <- nextMove rnr
+    nextT <- nextTile tr
+    let nextTrack = if canPass nextM nextT then tail tr else tr
+    return nextTrack
+
+changeState (runner, track, currentRank) = tryTrack runner track
+    
+    -- (
+    -- runner {energy = (pred $ energy runner), moveSet = tail (moveSet runner)}
+    -- , undefined
+    -- , currentRank {rank = succ $ rank currentRank} )
+
+-- type RunnerApp = ExceptT String (State (Runner, Track, RunnerRank)) ()
+-- runApp = runState . runExceptT
+-- makeMove :: RunnerApp
+-- makeMove = do
+--     (runner, track, rank) <- get
+--     when (trackEnded track) (throwError "Track ended")
+--     when (noEnergy runner || noMoves runner) (throwError $ "Runner done: " ++ show runner)
+--     modify changeState
 
