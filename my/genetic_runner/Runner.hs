0f54696c4f95dda70c7e0ef56139dc5d8fdbac16
Mikhel <m282021@gmail.com>
2019-02-22 15:26:16 +0300

    ...

--- my/genetic_runner/Runner.hs
+++ my/genetic_runner/Runner.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 import Data.List
 import Control.Monad
 import Control.Monad.State
@@ -10,26 +12,28 @@ type Track = [Tile]
 data Move = Run | Jump | DoubleJump deriving Show
 data Runner = Runner {rID :: String, energy :: Int, moveSet :: [Move]} deriving Show
 data RunnerRank = RunnerRank {runnerId :: String, rank :: Int} deriving Show
+type RunState = (Runner, [Tile], RunnerRank)
 
 testTrack = [Flat, Flat, Peak, Rock, Flat]
 testRunner = Runner {
     rID = "r1"
     , energy = 6
-    , moveSet = [Run, Run, Run, Run]
+    , moveSet = [Run, Run, Run, Run, Run, Run, Run, Run]
 }
 initRank runner = RunnerRank (rID testRunner) 0
 testState = (testRunner, testTrack, initRank testRunner)
 
-noEnergy rnr | energy rnr == 0 = True
-             | otherwise = False
-noMoves = null . moveSet
+tickMoveset rnr = case moveSet rnr of
+    [] -> Left "Runner no moves"
+    m:ms -> Right (m,ms)
 
-nextMove rnr = case moveSet rnr of
-    [] -> Left "No moves left"
-    x:_ -> Right x
-nextTile tr = case tr of
+tickEnergy rnr = let nxt = pred (energy rnr) in case nxt of
+    0 -> Left "Runner no energy"
+    x -> Right x
+
+tickTrack tr = case tr of
     [] -> Left "Track ended"
-    x:_ -> Right x
+    t:ts -> Right (t,ts)
 
 canPass :: Move -> Tile -> Bool 
 canPass Run Flat = True
@@ -40,28 +44,38 @@ canPass _ _ = False
 trackEnded :: [Tile] -> Bool
 trackEnded = null
 
-tickRunner rnr = rnr {energy = (pred $ energy rnr), moveSet = tail (moveSet rnr)}
-tickRank cr = cr {rank = succ $ rank cr}
+-- makeStep rnr tr = do
+--     nextEnrg <- tickEnergy rnr
+--     (nextM, nextMs) <-  tickMoveset rnr
+--     (nextT, restT) <- tickTrack tr
+--     let nextTrack = if canPass nextM nextT then restT else tr
+--     return (rnr {energy = nextEnrg, moveSet = nextMs}, nextTrack)
+
+
+-- todo: better validated here
+tickRunner rnr = do
+    nextEnrg <- tickEnergy rnr
+    (_, nextMs) <-  tickMoveset rnr
+    return $ rnr {energy = nextEnrg, moveSet = nextMs}
+
+upRank rnk = rnk {rank = succ $ rank rnk}
 
 tryTrack rnr tr = do
-    nextM <- nextMove rnr
-    nextT <- nextTile tr
+    (nextM, _) <- tickMoveset rnr
+    (nextT, _) <- tickTrack tr
     let nextTrack = if canPass nextM nextT then tail tr else tr
     return nextTrack
 
-changeState (runner, track, currentRank) = tryTrack runner track
-    
-    -- (
-    -- runner {energy = (pred $ energy runner), moveSet = tail (moveSet runner)}
-    -- , undefined
-    -- , currentRank {rank = succ $ rank currentRank} )
+changeState (runner, track, rnk) = liftA3 (,,) (tickRunner runner) (tryTrack runner track) (pure $ upRank rnk)
 
--- type RunnerApp = ExceptT String (State (Runner, Track, RunnerRank)) ()
--- runApp = runState . runExceptT
--- makeMove :: RunnerApp
--- makeMove = do
---     (runner, track, rank) <- get
---     when (trackEnded track) (throwError "Track ended")
---     when (noEnergy runner || noMoves runner) (throwError $ "Runner done: " ++ show runner)
---     modify changeState
+newtype RunnerApp a = RunnerApp {unApp :: ExceptT String (State (Runner, Track, RunnerRank)) a} 
+    deriving (Functor, Applicative, Monad, MonadState RunState, MonadError String)
+runApp = runState . runExceptT . unApp
+
+makeMove :: RunnerApp ()
+makeMove = get >>= either throwError put . changeState
+
+test1 = runApp (forever makeMove) testState
+test2 = runApp (replicateM 4 makeMove) testState
+
 
