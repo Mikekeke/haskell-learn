cc51d614487e9d13edd373c1d1862136b76e8890
Mike L <m282021@gmail.com>
2019-03-04 18:12:27 +0300

    ...

--- tests/TestManyFailedBinds.hs
+++ tests/TestManyFailedBinds.hs
@@ -8,8 +8,7 @@ instance Applicative Mb where {pure = return; (<*>) = ap}
 instance Monad Mb where 
     return = Ok
     (Ok x) >>= k = trace "Ok bind" $ k x
-    Nok >>= _ = trace "Nok bind" Nok -- Short circuit: no call to 'k', no code in 'k' run
--- !!! remember about short circuit, you checking this 2nd time already
+    Nok >>= _ = trace "Nok bind" Nok
 
 testFun :: Enum a => a -> Mb a
 testFun = Ok . succ
@@ -25,4 +24,13 @@ Ok 3
 λ: Nok >>= kArrsCmps
 Nok bind
 Nok
+-}
+
+-- BUT if not composed:
+testNotComposed = Nok >>= return >>= return
+{-
+λ: testNotComposed 
+Nok bind
+Nok bind
+Nok    
 -}
