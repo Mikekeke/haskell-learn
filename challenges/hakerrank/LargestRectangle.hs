9cc328f8289739384016cd25219fee55689cf499
Mikhel <m282021@gmail.com>
2019-03-25 22:42:14 +0300

    ...

--- challenges/hakerrank/LargestRectangle.hs
+++ challenges/hakerrank/LargestRectangle.hs
@@ -1,6 +1,6 @@
 -- https://www.hackerrank.com/challenges/largest-rectangle/problem
--- times out
 
+-- 1st idea: times out
 getRect l (i, x) =  let
         getLen = length . takeWhile (>= x)
         (left, right) = splitAt i l
@@ -8,4 +8,21 @@ getRect l (i, x) =  let
         r' = getLen right
     in x * (l'+r')
 
-gg = maximum . (map <$> getRect <*> zip [0..])
+largestRectangle1 = maximum . (map <$> getRect <*> zip [0..])
+
+-- 2nd idea: passed tests
+chop :: Eq a => a -> [a] -> [[a]]
+chop _ [] = [[]]
+chop s (x:xs) = if s == x 
+    then [] : chop s xs 
+    else let (xs':ys) = chop s xs in (x:xs') : ys
+
+largestRectangle2 :: [Int] -> Int
+largestRectangle2 [x] = x
+largestRectangle2 l = let
+        ln = length l
+        lo = minimum l
+        sq = (lo * ln) 
+        chopped = filter (not . null) $ chop lo l
+    in if null chopped then sq else max sq ( maximum $ largestRectangle2 <$> chopped)
+    
